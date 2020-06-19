d0138cba187fabede9757fb179cbb89829a0a7c6
brad taniguchi <bradtaniguchi@gmail.com>
2020-06-19 14:24:32 -0700

    Add utility functions

--- index.js
+++ index.js
@@ -1,4 +1,54 @@
-module.exports = function checkCashRegister(price, cash, cid) {
-  var change;
-  return {};
+/**
+ * Map of the "cash-type" to their pennies
+ * amount
+ */
+const penniesMap = {
+  PENNY: 1,
+  NICKEL: 5,
+  DIME: 10,
+  QUARTER: 25,
+  ONE: 100,
+  FIVE: 500,
+  TEN: 1000,
+  TWENTY: 20000,
+  'ONE HUNDRED': 100000,
 };
+/**
+ * Returns
+ * @param {number} price the cost of the item
+ * @param {*} cash the amount given
+ * @param {*} cid the cash in drawer
+ * @returns {string} .status the status of the drawer, could be
+ *   - INSUFFICIENT_FUNDS
+ *   - CLOSED
+ *   - OPEN
+ * @returns {array} .change the list of change in bills/coins
+ */
+module.exports = function checkCashRegister(price, cash, cid) {
+  const penniesInDrawer = getPenniesInDrawer(cid);
+  if (!hasFunds(price, penniesInDrawer)) {
+    return {
+      STATUS: 'INSUFFICIENT_FUNDS',
+      change: [],
+    };
+  }
+  const changeInPennies = getPennies(cash) - getPennies(price);
+  // TODO:
+  return changeInPennies;
+};
+/**
+ * Returns the pennies for the given price
+ */
+const getPennies = (price) => Math.round(price * 100);
+/**
+ * Returns the pennies in the drawer from the cash in the drawer
+ */
+const getPenniesInDrawer = (cid) =>
+  cid.reduce((acc, [kind, amount]) => [...acc, [kind, getPennies(amount)]], []);
+
+/**
+ * Returns if the pennies in the drawer meets the price
+ */
+const hasFunds = (price, penniesInDrawer) =>
+  penniesInDrawer.reduce((acc, [_, pennies]) => (acc += pennies), 0) >=
+  getPennies(price);
--- test.js
+++ test.js
@@ -16,9 +16,20 @@ describe('checkCashRegister', () => {
         ['ONE HUNDRED', 100],
       ])
     ).toEqual('object'));
-  test.todo(
-    'checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) should return {status: "OPEN", change: [["QUARTER", 0.5]]}.'
-  );
+  test('checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) should return {status: "OPEN", change: [["QUARTER", 0.5]]}.', () =>
+    expect(
+      checkCashRegister(19.5, 20, [
+        ['PENNY', 1.01],
+        ['NICKEL', 2.05],
+        ['DIME', 3.1],
+        ['QUARTER', 4.25],
+        ['ONE', 90],
+        ['FIVE', 55],
+        ['TEN', 20],
+        ['TWENTY', 60],
+        ['ONE HUNDRED', 100],
+      ])
+    ).toEqual({ status: 'OPEN', change: [['QUARTER', 0.5]] }));
   test.todo(
     'checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) should return {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}.'
   );
